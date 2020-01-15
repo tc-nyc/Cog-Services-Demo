0087b32f32d2e259d34756f58884d9925f0df8ec
tc-nyc <Rhodeisland2*>
2020-01-15 14:15:05 -0800

    Added new Image File
    Also updated the python script

--- image_convert.py
+++ image_convert.py
@@ -9,6 +9,8 @@ import os
 from PIL import Image
 import sys
 import time
+import pandas
+
 
 #create enviroment variables on your OS using this guide https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-apis-create-account?tabs=multiservice%2Cwindows#configure-an-environment-variable-for-authentication
 
@@ -39,7 +41,7 @@ This API call can also recognize handwriting (not shown).
 '''
 print("===== Batch Read File - remote =====")
 # Get an image with printed text
-remote_image_printed_text_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/printed_text.jpg"
+remote_image_printed_text_url = "http://photos1.blogger.com/blogger/3880/1489/1600/1918%20Standings.jpg"
 
 # Call API with URL and raw response (allows you to get the operation location)
 recognize_printed_results = computervision_client.batch_read_file(remote_image_printed_text_url,  raw=True)
@@ -61,5 +63,5 @@ if get_printed_text_results.status == TextOperationStatusCodes.succeeded:
     for text_result in get_printed_text_results.recognition_results:
         for line in text_result.lines:
             print(line.text)
-            print(line.bounding_box)
+            #finalDataset = pandas.DataFrame(line.text, columns = line.bounding_box)
 print()
